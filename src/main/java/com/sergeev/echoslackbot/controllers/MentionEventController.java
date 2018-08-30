7ea3be0ba93d1aeae866ba9dc35e4ea8998b7ecc
Morgen Gramlich <danila.sergeyev@gmail.com>
2018-08-30 11:42:36 +0300

    Cosmetic fix: remove unused imports

--- src/main/java/com/sergeev/echoslackbot/controllers/MentionEventController.java
+++ src/main/java/com/sergeev/echoslackbot/controllers/MentionEventController.java
@@ -3,12 +3,10 @@ package com.sergeev.echoslackbot.controllers;
 import com.sergeev.echoslackbot.models.SlackMessage;
 import com.sergeev.echoslackbot.services.api.ResponseService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
 
 @RestController
 public class MentionEventController {
